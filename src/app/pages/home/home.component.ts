eef9d884048f358db756c7748bc085cc9b6c165f
gefoxer <kiparisina620@gmail.com>
2025-06-04 23:56:42 +0300

    card/card_sale/adaptive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { RouterLink } from '@angular/router';
 import { FooterComponent } from "../../components/footer/footer.component";
+import { CardComponent } from '../../components/card/card.component';
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
